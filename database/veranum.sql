51333ed01f3cee08767d07e345af62a27bc9426b
Angel <an.bustamantez@duocuc.cl>
2025-06-26 10:47:50 -0400

    Se arreglo el Archivo veranum.sql

--- database/veranum.sql
+++ database/veranum.sql
@@ -40,6 +40,21 @@ INSERT INTO `clientes` (`nombre`, `email`, `password`, `role`) VALUES
 	('Gerente', 'gerente@demo.com', '$2y$10$Qe6Qw1Qw1Qw1Qw1Qw1Qw1uQw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1', 'gerente'),
 	('Cliente Demo', 'cliente@demo.com', '$2y$10$Qe6Qw1Qw1Qw1Qw1Qw1Qw1uQw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1Qw1', 'cliente');
 
+-- Volcando estructura para tabla veranum.hoteles
+CREATE TABLE IF NOT EXISTS `hoteles` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) NOT NULL,
+  `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Volcando datos para la tabla veranum.hoteles: ~1 rows (aproximadamente)
+DELETE FROM hoteles;
+INSERT INTO `hoteles` (`id`, `nombre`, `creado_en`) VALUES
+	(1, 'Veranum Santiago', NOW()),
+	(2, 'Veranum Vta Región', NOW());
+
+
 -- Volcando estructura para tabla veranum.habitaciones
 CREATE TABLE IF NOT EXISTS `habitaciones` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -60,49 +75,6 @@ INSERT INTO `habitaciones` (`id`, `hotel_id`, `nombre`, `precio`, `foto`, `cread
 	(2, 1, 'Doble Standard', 30000.00, '', NOW()),
 	(3, 2, 'Matrimonial Vista Mar', 60000.00, '', NOW()),
 	(4, 2, 'Single Económica', 20000.00, '', NOW());
-
--- Volcando estructura para tabla veranum.hoteles
-CREATE TABLE IF NOT EXISTS `hoteles` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) NOT NULL,
-  `creado_en` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Volcando datos para la tabla veranum.hoteles: ~1 rows (aproximadamente)
-DELETE FROM hoteles;
-INSERT INTO `hoteles` (`id`, `nombre`, `creado_en`) VALUES
-	(1, 'Veranum Santiago', NOW()),
-	(2, 'Veranum Vta Región', NOW());
-
--- Volcando estructura para tabla veranum.insumos
-CREATE TABLE IF NOT EXISTS `insumos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `unidad` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `stock_actual` decimal(10,2) NOT NULL DEFAULT '0.00',
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Volcando datos para la tabla veranum.insumos: ~0 rows (aproximadamente)
-
--- Volcando estructura para tabla veranum.movimientos_insumo
-CREATE TABLE IF NOT EXISTS `movimientos_insumo` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_insumo` int NOT NULL,
-  `tipo_movimiento` enum('ingreso','consumo') COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cantidad` decimal(10,2) NOT NULL,
-  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  `observacion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_insumo` (`id_insumo`),
-  CONSTRAINT `movimientos_insumo_ibfk_1` FOREIGN KEY (`id_insumo`) REFERENCES `insumos` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- Volcando datos para la tabla veranum.movimientos_insumo: ~0 rows (aproximadamente)
 
 -- Volcando estructura para tabla veranum.reservas
 CREATE TABLE IF NOT EXISTS `reservas` (
@@ -126,6 +98,8 @@ INSERT INTO `reservas` (`id`, `cliente_id`, `habitacion_id`, `fecha_inicio`, `fe
 	(1, 5, 1, '2025-06-23', '2025-06-25', 'checkin', NOW()),
 	(2, 5, 3, '2025-06-26', '2025-06-28', 'pendiente', NOW()),
 	(3, 5, 2, '2025-06-20', '2025-06-22', 'checkout', NOW());
+
+
 
 -- Volcando estructura para tabla veranum.servicios
 CREATE TABLE IF NOT EXISTS `servicios` (
@@ -167,6 +141,39 @@ INSERT INTO `tipos_servicio` (`id`, `nombre`, `precio`, `created_at`, `updated_a
 	(2, 'Acceso a Spa', 25000.00, NOW(), NOW()),
 	(3, 'Lavandería', 5000.00, NOW(), NOW()),
 	(4, 'Botella de Vino', 15000.00, NOW(), NOW());
+
+
+-- Volcando estructura para tabla veranum.insumos
+CREATE TABLE IF NOT EXISTS `insumos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `descripcion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `unidad` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `stock_actual` decimal(10,2) NOT NULL DEFAULT '0.00',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla veranum.insumos: ~0 rows (aproximadamente)
+
+-- Volcando estructura para tabla veranum.movimientos_insumo
+CREATE TABLE IF NOT EXISTS `movimientos_insumo` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `id_insumo` int NOT NULL,
+  `tipo_movimiento` enum('ingreso','consumo') COLLATE utf8mb4_unicode_ci NOT NULL,
+  `cantidad` decimal(10,2) NOT NULL,
+  `fecha` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `observacion` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_insumo` (`id_insumo`),
+  CONSTRAINT `movimientos_insumo_ibfk_1` FOREIGN KEY (`id_insumo`) REFERENCES `insumos` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Volcando datos para la tabla veranum.movimientos_insumo: ~0 rows (aproximadamente)
+
+
+
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
